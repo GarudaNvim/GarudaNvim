fbb740f1dfa7533c3929e9d6cc215c918b0a97cf
prrockzed <priyanshuranjan2806@gmail.com>
2024-10-12 00:18:33 +0530

    Added lualine plugin for statusline

--- lua/garudanvim/plugin_config/init.lua
+++ lua/garudanvim/plugin_config/init.lua
@@ -5,3 +5,4 @@ require("garudanvim.plugin_config.color_config")
 -- FILES
 require("garudanvim.plugin_config.nvimtree")
 require("garudanvim.plugin_config.barbar")
+require("garudanvim.plugin_config.lualine")
--- lua/garudanvim/plugin_manager/dump_plugins.lua
+++ lua/garudanvim/plugin_manager/dump_plugins.lua
@@ -23,6 +23,12 @@ local dump_plugins = {
 		version = "^1.0.0", -- optional: only update when a new 1.x version is released
 	},
 
+  -- status line at the bottom
+	{
+		"nvim-lualine/lualine.nvim",
+		lazy = false,
+	},
+
   -- dependencies
   {
     "nvim-tree/nvim-web-devicons",
